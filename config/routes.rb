1d20152e3590838d05587aac2e9223733b8c6ec9
John Doe <paytonanthony2020@gmail.com>
2021-05-26 18:58:54 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #  allows us to communicte through the browser to our app
+  #  rails command, http verbs, url, endpoint
+  #  resources -it will gen, all needed routes for the main actions
+  # resources :controller
+  # root -onces , this root page
+  # root 'controller@action'
+  # long ways, defining your own routes
+  # http verb, url, endpoint, rails command(optional)
+  # get 'people/new', to: 'people#index'
+  # post etc.
+  root 'subs#index'
+
+  # bad example
+  # resources :subs do #faster creating routes
+  #   resources :topics do
+  #     resources :comments
+  #   end
+  # end
+
+  resources :subs do #faster creating routes
+    resources :topics
+  end
+
+  resources :topics do
+    resources :comments
+  end
+  # get 'sub/1'
+
+  # get 'sub/3'
+  # get 'sub/:id' BEST WAY FOR THE ID
+  # :id - a placeholder for the id
+  # :parent_id
+  # embed, if there are any relational controllers we need to embed the 
+
 end
